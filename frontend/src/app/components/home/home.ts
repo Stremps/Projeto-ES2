90a7c632f497453b867dea3ec50ee7c70c4bf0b8
JosefinoXp <joselucasxp345@gmail.com>
2025-10-14 23:02:46 -0300

    Nova Página Inicial Pronto | Novas imagens em public |

--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [ CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+  constructor() {}
 }
